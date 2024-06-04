b3710099328be69b3411c5607dc7b5fb29a863c7
suraj <alphaguy433@gmail.com>
2024-06-04 23:42:31 +0530

    Basics of python language

---
+++ Basics/_01_basics.py
@@ -0,0 +1,43 @@
+# Printing Different values
+print('Hello World')
+print(2024)
+print(True)
+print([2,4,6,8])
+print({'name':'suraj'})
+
+# $$$$$ Datatypes in python  $$$$$$
+#$$ Numeric $$
+#Integer
+num=46
+print("Integer: ",num)
+
+#Float
+pi=3.14
+print("float", pi)
+
+#Complex
+com_num = 3+5J
+print("Complex number: ", com_num)
+print("Complex number: ", type(com_num))
+
+#$$ Sequence $$
+#String
+str="Engineer"
+str2='''Code
+        Everyday'''
+print("String: ",str)
+print("MutliLine String: ",str2)
+
+#List
+#List is similar to array but items can be different types
+list_bike=['pulsar', 'royal', 'duke']
+print("List items:",list_bike[1])
+
+#Tuple
+#tuple is ordered collection and immutable
+tuple_mobile=("samsung","oneplus","moto")
+tuple_odd =(1,3,5,7,9)
+print("tuples: ", tuple_mobile)
+print('tuple num: ', tuple_odd)
+
+
